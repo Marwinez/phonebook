9f28f5bdbe5dd78e24f33fcca22d6d1d15965a69
Marwinez <gietus579@gmail.com>
2024-09-16 16:18:35 +0200

    Updated frontend, added more restrictions while validating new person info

--- modules/person.js
+++ modules/person.js
@@ -12,8 +12,22 @@ mongoose.connect(url).then(result => {
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: [ 3, 'Name must have at least 3 characters' ],
+        required: [ true, 'Person name is required' ]
+    },
+    number: {
+        type: String,
+        minLength: [ 8, 'Phone number must have at least 8 characters' ],
+        validate: {
+            validator: function(number) {
+                return /^\d{2,3}-\d{5,}$/gm.test(number);
+            }, 
+            message: `Number must match structure: 2-3 numbers, '-' sign, at least 5 more numbers\nexamples: 12-345678, 123-45678`
+        },
+        required: [ true, 'Phone number is required' ]
+    }
 })
 
 personSchema.set('toJSON', {
