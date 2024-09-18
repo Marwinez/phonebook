fa9647f74835ef92dbbf31782751bf373c3046a2
Marwinez <gietus579@gmail.com>
2024-09-18 14:07:56 +0200

    Added eslint for code style verification

--- index.js
+++ index.js
@@ -6,90 +6,90 @@ const cors = require('cors')
 const Person = require('./modules/person')
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'uknown endpoint' })
+  response.status(404).send({ error: 'uknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token('person', (req, res) => Object.keys(req.body).length !== 0 ? JSON.stringify(req.body) : null)
+morgan.token('person', (req/*, res*/) => Object.keys(req.body).length !== 0 ? JSON.stringify(req.body) : null)
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens['person'](req, res)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['person'](req, res)
+  ].join(' ')
+}))
 
 
 app.use(express.json())
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
-    })
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id).then(result => {
-        response.status(204).end()
-    })
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  })
     .catch(error => next(error))
-}) 
+})
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    console.log(body)
+  const body = request.body
+  console.log(body)
 
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    
-    Person.findByIdAndUpdate(request.params.id, person)
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person)
     .then(result => response.json(result))
     .catch(error => next(error))
 })
--- modules/person.js
+++ modules/person.js
@@ -4,38 +4,38 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-mongoose.connect(url).then(result => {
-    console.log('connected to MongoDB')
+mongoose.connect(url).then(() => {
+  console.log('connected to MongoDB')
 })
-.catch(error => {
+  .catch(error => {
     console.log('error connecting to MongoDB: ', error.message)
-})
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: [ 3, 'Name must have at least 3 characters' ],
-        required: [ true, 'Person name is required' ]
+  name: {
+    type: String,
+    minLength: [ 3, 'Name must have at least 3 characters' ],
+    required: [ true, 'Person name is required' ]
+  },
+  number: {
+    type: String,
+    minLength: [ 8, 'Phone number must have at least 8 characters' ],
+    validate: {
+      validator: function(number) {
+        return /^\d{2,3}-\d{5,}$/gm.test(number)
+      },
+      message: 'Number must match structure: 2-3 numbers, '-' sign, at least 5 more numbers\nexamples: 12-345678, 123-45678'
     },
-    number: {
-        type: String,
-        minLength: [ 8, 'Phone number must have at least 8 characters' ],
-        validate: {
-            validator: function(number) {
-                return /^\d{2,3}-\d{5,}$/gm.test(number);
-            }, 
-            message: `Number must match structure: 2-3 numbers, '-' sign, at least 5 more numbers\nexamples: 12-345678, 123-45678`
-        },
-        required: [ true, 'Phone number is required' ]
-    }
+    required: [ true, 'Phone number is required' ]
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
